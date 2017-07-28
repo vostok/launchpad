48b715c239281193e57cc4210a29c0aec1a08560
spaceorc <spaceorc@skbkontur.ru>
2017-07-28 21:10:46 +0500

    Add webapi template project

---
+++ templates/webapi/ProjectTemplate/Program.cs
@@ -0,0 +1,16 @@
+﻿using Topshelf;
+
+namespace ProjectTemplate
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            HostFactory.Run(x =>
+            {
+                x.SetServiceName(Settings.ServiceName);
+                x.Service<WebApiService>();
+            });
+        }
+    }
+}
---
+++ templates/webapi/ProjectTemplate/WebApiService.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using JetBrains.Annotations;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Topshelf;
+
+namespace ProjectTemplate
+{
+    internal class WebApiService : ServiceControl
+    {
+        public bool Start([NotNull] HostControl hostControl)
+        {
+            WebApp.Start(Settings.ListenPrefix, builder =>
+            {
+                var config = new HttpConfiguration();
+                config.MapHttpAttributeRoutes();
+                builder.UseWebApi(config);
+            });
+            return true;
+        }
+
+        public bool Stop([NotNull] HostControl hostControl)
+        {
+            return true;
+        }
+    }
+}
