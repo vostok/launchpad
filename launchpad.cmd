4ce86f645507db143b4e8687afb58ea23ead3197
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2017-10-10 17:47:11 +0500

    Fix cmd script

--- launchpad.cmd
+++ launchpad.cmd
@@ -1,7 +1,11 @@
 @ECHO OFF
 
-IF NOT EXIST %~dp0bin\Launchpad.dll (
-	CALL dotnet publish -c Release -o ..\..\bin\ src\Launchpad\Launchpad.csproj  || EXIT /B 1
+pushd %~dp0
+
+IF NOT EXIST bin\Launchpad.dll (
+	dotnet publish -c Release -o ..\..\bin\ src\Launchpad\Launchpad.csproj || EXIT /B 1
 )
 
-%~dp0dotnet bin\Launchpad.dll %*
+dotnet bin\Launchpad.dll %*
+
+popd
