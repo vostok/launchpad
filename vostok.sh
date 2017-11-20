15b223874bef86b7932fdb409daffd34a3966675
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2017-11-20 18:16:46 +0500

    Fix vostok.sh for MacOS (#8)

--- vostok.sh
+++ vostok.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-DIR="$(dirname $(readlink -f $0))"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  DIR="$(dirname $(readlink $0))"
+else
+  DIR="$(dirname $(readlink -f $0))"
+fi
+
 EXEC="$DIR/bin/Launchpad.dll"
 PROJECT="$DIR/src/Launchpad/Launchpad.csproj"
-
 
 if ! [ -f "$EXEC" ]
 then
